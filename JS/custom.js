1590203681a3a6a42e790e3297b96083826fc4f6
MK Hasan <55194938+MKHasandev@users.noreply.github.com>
2022-05-30 02:16:44 +0600

    portfolio

--- JS/custom.js
+++ JS/custom.js
@@ -42,9 +42,41 @@ $(document).ready(function(){
         $('.testimonial-slider').slick('slickNext');
     });
 
+    // if($("body").hasClass("side-header")){
+    //     $('.smooth-scroll').on('click', function() {
+    //         event.preventDefault();
+    //         var sectionTo = $(this).attr('href');
+    //         $('html, body').stop().animate({
+    //           scrollTop: $(sectionTo).offset().top}, 1500, 'easeInOutExpo');
+    //     });
+    //        }else {
+    //     $('.smooth-scroll').on('click', function() {
+    //         event.preventDefault();
+    //         var sectionTo = $(this).attr('href');
+    //         $('html, body').stop().animate({
+    //           scrollTop: $(sectionTo).offset().top - 50}, 1500, 'easeInOutExpo');
+    //     });
+    //     }
+
 
     // portfolio
-    $('#mixitup-items').mixItUp();
+    // $('.portfolio-target').mixItUp({
+    //     animation: {
+    //         enable: true,
+    //         effects: 'fade scale',
+    //         duration: 600,
+    //         easing: 'ease',
+    //         perspectiveDistance: '3000px',
+    //         perspectiveOrigin: '50% 50%',
+    //         queue: true,
+    //         queueLimit: 1,
+    //         animateChangeLayout: false,
+    //         animateResizeContainer: true,
+    //         animateResizeTargets: false,
+    //         staggerSequence: null,
+    //         reverseOut: false
+    //     }
+    // });
 
 
    
